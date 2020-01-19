70f29db1a517884e212fdf2c6772628884b55a4f
Pedro Casas <pedrocasas@Carmines-MacBook-Air.local>
2020-01-19 13:53:25 -0500

    Space test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-// IMport React and React-DOM libraries
+// Import React and React-DOM libraries
+import React from 'react';
+imort ReactDOM from 'react-dom';
+
+//Create React component
+
+// Take the React component and show it on the screen
