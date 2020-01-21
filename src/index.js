2c5d693ee62d33cf67c393f5771c4e7bd1790719
Pedro Casas <pedrocasas@Carmines-MacBook-Air.local>
2020-01-20 20:25:12 -0500

    Modularized comments

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 // Import React and React-DOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker'
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 //Create React component
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()}/>
-        </a>
-
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">
-            Nice Blog Post!
-          </div>
-        </div>
-      </div>
+      <CommentDetail/>
     </div>
   )
 };
