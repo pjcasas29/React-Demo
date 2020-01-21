9d59e560fb1a7918077d43b8f3d1ffdfe82732c3
Pedro Casas <pedrocasas@Carmines-MacBook-Air.local>
2020-01-20 20:14:45 -0500

    Added comment code and semantic ui css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 // Import React and React-DOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import faker from 'faker'
 //Create React component
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()}/>
+        </a>
 
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">
+            Nice Blog Post!
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
 // Take the React component and show it on the screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
